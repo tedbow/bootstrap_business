05ec1d31f8cb5d5c8db93e4f3cae971dedccbd4f
Stavros Kounis <skounis@gmail.com>
2016-02-09 13:04:09 +0200

    Fix the compatibility with the final D8 stable release

--- color/preview.js
+++ color/preview.js
@@ -16,24 +16,24 @@
       }
 
     // Main-bg
-    $('#preview', form).css('background-color', $('#palette input[name="palette[bg]"]', form).val());
+    $('#preview', form).css('background-color', $('.js-color-palette input[name="palette[bg]"]', form).val());
       
-    $('#preview-main #preview-node-footer', form).css('border-top-color', $('#palette input[name="palette[bg]"]', form).val());
+    $('#preview-main #preview-node-footer', form).css('border-top-color', $('.js-color-palette input[name="palette[bg]"]', form).val());
 
     // Text preview
-    $('#preview a, #preview a:hover, #preview a:focus', form).css('color', $('#palette input[name="palette[link]"]', form).val());
+    $('#preview a, #preview a:hover, #preview a:focus', form).css('color', $('.js-color-palette input[name="palette[link]"]', form).val());
     $('#preview h2 a', form).css('color', '#444');
     $('#preview ul.preview-menu li a', form).css('color', '#999999');  
     $('.preview-call-to-action .preview-btn', form).css('color', '#ffffff');
-    $('#preview ul.preview-menu li a.preview-active', form).css('color', $('#palette input[name="palette[link]"]', form).val());
+    $('#preview ul.preview-menu li a.preview-active', form).css('color', $('.js-color-palette input[name="palette[link]"]', form).val());
 
     // Pre Header
-    $('.preview-toggle-control span', form).css('background-color', $('#palette input[name="palette[highlight]"]', form).val());
-    $('.preview-toggle-control', form).css('border-top-color', $('#palette input[name="palette[highlight]"]', form).val());
+    $('.preview-toggle-control span', form).css('background-color', $('.js-color-palette input[name="palette[highlight]"]', form).val());
+    $('.preview-toggle-control', form).css('border-top-color', $('.js-color-palette input[name="palette[highlight]"]', form).val());
 
     // Call-to-action
-    $('#preview-page-bottom', form).css('background-color', $('#palette input[name="palette[highlight]"]', form).val());
-    $('.preview-call-to-action .preview-btn', form).css('background-color', $('#palette input[name="palette[link]"]', form).val());
+    $('#preview-page-bottom', form).css('background-color', $('.js-color-palette input[name="palette[highlight]"]', form).val());
+    $('.preview-call-to-action .preview-btn', form).css('background-color', $('.js-color-palette input[name="palette[link]"]', form).val());
 
     }
   };
